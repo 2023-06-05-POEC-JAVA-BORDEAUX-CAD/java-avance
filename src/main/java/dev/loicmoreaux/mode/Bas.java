1da14ae1d96be3461862d1c3d2e701bb8d0f7964
Loic2dot0 <moreaux.loic@gmail.com>
2023-06-28 16:52:11 +0200

    add Bas

---
+++ src/main/java/dev/loicmoreaux/mode/Bas.java
@@ -0,0 +1,45 @@
+package dev.loicmoreaux.mode;
+
+public class Bas implements Vetement{
+	private String nom;
+	private Integer longueur;
+	
+	/*
+	 * Constructor
+	 */
+	public Bas() {}
+	
+	public Bas(String nom, Integer longueur) {
+		this.nom = nom;
+		this.longueur = longueur;
+	}
+	
+	/*
+	 * Getters
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	public Integer getLongueur() {
+		return longueur;
+	}
+
+	/*
+	 * Setters
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setLongueur(Integer longueur) {
+		this.longueur = longueur;
+	}
+
+	@Override
+	public void affiche() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
