d14c44618745d7d7d455194295762bd1562b6d98
Loic2dot0 <moreaux.loic@gmail.com>
2023-06-28 16:44:22 +0200

    add haut

---
+++ src/main/java/dev/loicmoreaux/mode/Haut.java
@@ -0,0 +1,34 @@
+package dev.loicmoreaux.mode;
+
+public class Haut implements Vetement {
+	/*
+	 * Declaration attributes
+	 */
+	private String nom;
+	
+	/*
+	 * Constructor
+	 */
+	public Haut() {}
+	
+	/*
+	 * Getters
+	 */
+	public String getNom() {
+		return nom;
+	}
+	
+	/*
+	 * Setters
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public void affiche() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
