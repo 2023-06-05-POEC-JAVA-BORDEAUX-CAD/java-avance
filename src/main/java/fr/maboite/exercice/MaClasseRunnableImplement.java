c71dc04fbff56bfe1b47ff01cecb6725035d97ec
Loic2dot0 <moreaux.loic@gmail.com>
2023-06-28 11:48:02 +0200

    add MaClasseRunnableImplement

---
+++ src/main/java/fr/maboite/exercice/MaClasseRunnableImplement.java
@@ -0,0 +1,14 @@
+package fr.maboite.exercice;
+
+public class MaClasseRunnableImplement implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			System.out.println("Ceci est un Runnable Implement");
+		}
+	}
+	
+	
+}
