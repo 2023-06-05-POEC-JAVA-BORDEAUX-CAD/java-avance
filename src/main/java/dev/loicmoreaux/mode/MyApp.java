4a56f99ed58eb6cf47885c66ad86348583787fea
Loic2dot0 <moreaux.loic@gmail.com>
2023-06-28 22:48:12 +0200

    update MyApp

--- src/main/java/dev/loicmoreaux/mode/MyApp.java
+++ src/main/java/dev/loicmoreaux/mode/MyApp.java
@@ -6,9 +6,13 @@ public class MyApp {
 	
 		Haut haut1 = new Haut("Tshirt bleu");
 		Bas bas1 = new Bas("jean noir", 85);
+		Tshirt tshirt = new Tshirt("Tshirt noir");
 		Client client1 = new Client("Doe", "John", haut1, bas1);
 		
+		Client client2 = new Client("Wake", "Alan", tshirt, bas1);
+		
 		client1.parle();
+		client2.parle();
 		
 	}	
 }
