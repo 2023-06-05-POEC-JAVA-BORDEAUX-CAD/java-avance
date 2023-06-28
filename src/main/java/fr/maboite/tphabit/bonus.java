992f493f41574537fbb5b00f53ccf96b5e158e9d
Angelisium <angelisium@daum.net>
2023-06-28 16:38:02 +0200

    Update bonus.java

--- src/main/java/fr/maboite/tphabit/bonus.java
+++ src/main/java/fr/maboite/tphabit/bonus.java
@@ -5,9 +5,16 @@ public class bonus {
         Haut tshirt = new TShirt();
         tshirt.setNom("TSHIRT 1");
 
+        Haut chemise = new Chemise();
+        chemise.setNom("CHEMISE 1");
+
         Bas jupe = new Jupe();
         jupe.setNom("JUPE 1");
         jupe.setLongueur(true);
+
+        Bas pantalon = new Pantalon();
+        pantalon.setNom("PANTALON 1");
+        pantalon.setLongueur(true);
 
         Client c1 = new Client();
         c1.setNom("LeNom");
@@ -15,6 +22,14 @@ public class bonus {
         c1.setHaut(tshirt);
         c1.setBas(jupe);
 
+        Client c2 = new Client();
+        c2.setNom("LeNom 2");
+        c2.setPrenom("LePrenom 2");
+        c2.setHaut(chemise);
+        c2.setBas(pantalon);
+
         c1.parle();
+        System.out.println("------");
+        c2.parle();
     }
 }
