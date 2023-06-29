09a6950c4f1e7363e4065f64469d137003dfcc7e
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-06-29 11:36:18 +0200

    refactor: add before and after annotations

--- src/test/java/fr/maboite/AreaCalcTest.java
+++ src/test/java/fr/maboite/AreaCalcTest.java
@@ -2,18 +2,50 @@ package fr.maboite;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import fr.maboite.tests.*;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 public class AreaCalcTest {
+	AreaCalculator ac;
+	
+	
+	@BeforeAll
+	public static void beforeAll() {
+		System.out.println("before all");
+	}
+	
+	@BeforeEach
+	public void beforeEach() {
+		System.out.println("Nouveau test");
+		this.ac = new AreaCalculator();
+	}
+	
+	@AfterAll
+	public static void afterAll() {
+		System.out.println("tests finished");
+	}
 
+	@AfterEach
+	public void afterEach() {
+		System.out.println("test finished");
+	}
+	
 	@Test
 	public void square_area() {
-		AreaCalculator ac = new AreaCalculator();
+		
 		Integer expectedValue = ac.calcSquareArea(5);
 		int computedValue = 5*5;
 		assertEquals(expectedValue, computedValue);
