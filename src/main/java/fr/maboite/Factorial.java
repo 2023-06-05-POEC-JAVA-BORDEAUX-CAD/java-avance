d91d99571fbc2d89b3bbdec3a1cc4386c68e5a86
Loic2dot0 <moreaux.loic@gmail.com>
2023-06-29 16:56:51 +0200

    update factorial, passed all tests

--- src/main/java/fr/maboite/Factorial.java
+++ src/main/java/fr/maboite/Factorial.java
@@ -4,8 +4,15 @@ public class Factorial {
 
 	public Integer compute(int i) {
 		// TODO Auto-generated method stub
-		if(i <= 1) return 1;
-		return i;
+		Integer result = 1;
+		
+		if(i <= 1) return result;
+		
+		for(int j = 1; j <= i; j++) {
+			result *= j;
+		}
+		
+		return result;
 	}
 
 }
