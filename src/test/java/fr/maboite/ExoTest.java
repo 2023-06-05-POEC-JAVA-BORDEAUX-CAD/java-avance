1a161056da7ced13545d07566486a9ee2da7f412
Angelisium <angelisium@daum.net>
2023-06-29 11:54:02 +0200

    Update ExoTest.java

--- src/test/java/fr/maboite/ExoTest.java
+++ src/test/java/fr/maboite/ExoTest.java
@@ -1,11 +1,18 @@
 package fr.maboite;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class VoidClass {
     @Override
@@ -15,6 +22,27 @@ class VoidClass {
 }
 
 public class ExoTest {
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @BeforeEach
+    public void beforeEach(TestInfo test) {
+        System.out.println("    before each");
+        System.out.println("        execution de " + test.getDisplayName());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("    after each");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("after all");
+    }
+
     @Test
     public void toujoursVrai() {
         assertTrue(true);
