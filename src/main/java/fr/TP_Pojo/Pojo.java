aa0f3bc7f25c2c06b56d8a8f2e7e2debcc90e538
Noellie P <noellie.peuch@gmail.com>
2023-06-30 16:04:02 +0200

    WIP

---
+++ src/main/java/fr/TP_Pojo/Pojo.java
@@ -0,0 +1,32 @@
+package fr.TP_Pojo;
+
+public class Pojo {
+	private Integer id;
+	private String pojo;
+
+	public Pojo() {
+	}
+	
+	public Pojo(Integer id, String pojo) {
+		this.id = id;
+		this.pojo = pojo;
+	}
+
+	// Getters
+	public Integer getId() {
+		return id;
+	}
+
+	public String getPojo() {
+		return pojo;
+	}
+
+	// Setters
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setPojo(String pojo) {
+		this.pojo = pojo;
+	}
+}
---
+++ src/main/java/fr/TP_Pojo/PojoServlet.java
@@ -0,0 +1,53 @@
+package fr.TP_Pojo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Pojo", displayName="Pojo Servlet", urlPatterns = "/pojo", loadOnStartup = 1)
+public class PojoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setBufferSize(8192);
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>"
+                    + "<head><title>Générée par une servlet</title></head>");
+            out.println("<body  bgcolor=\"#ffffff\">"
+                    + "<h2>Quelle belle page générée dynamiquement !!!</h2>");
+            
+            String id = request.getParameter("id");
+            Integer id_pojo = Integer.parseInt("id");
+            if (id_pojo != null) {
+                Pojo pojo = new Pojo();
+                pojo.setId(id_pojo);
+            }
+            out.println("</body></html>");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "La première servlet .";
+
+    }
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Init terminé");
+	}
+    
+    
+}
