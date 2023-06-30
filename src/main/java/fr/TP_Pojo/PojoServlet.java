653b61da08fd3ead3b77282313700407373a618a
Noellie P <noellie.peuch@gmail.com>
2023-06-30 17:22:18 +0200

    C'est bon : json affiché

--- src/main/java/fr/TP_Pojo/PojoServlet.java
+++ src/main/java/fr/TP_Pojo/PojoServlet.java
@@ -9,45 +9,43 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name="Pojo", displayName="Pojo Servlet", urlPatterns = "/pojo", loadOnStartup = 1)
+@WebServlet(name = "Pojo", displayName = "Pojo Servlet", urlPatterns = "/pojo", loadOnStartup = 1)
 public class PojoServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    public void doGet(HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("application/json");
-        response.setBufferSize(8192);
-        try (PrintWriter out = response.getWriter()) {            
-            String id = request.getParameter("id");
-            Integer id_pojo = Integer.parseInt("id");
-            if (id_pojo != null) {
-                PojoDao pojoDao = new PojoDao();
-                Pojo test = pojoDao.getPojo(id_pojo);
-                Pojo pojo = new Pojo();
-                pojo.setId(id_pojo);
-                pojo.setPojo(test.getPojo());
-                PojoToJson pojoToJson = new PojoToJson();
-                String printPojo = pojoToJson.toJson(pojo);
-                out.println(printPojo);
-            }
-        }
-    }
+		response.setContentType("application/json");
+		response.setBufferSize(8192);
 
-    @Override
-    public String getServletInfo() {
-        return "La première servlet .";
+		String id = request.getParameter("id");
+		Integer id_pojo = Integer.parseInt(id);
 
-    }
+		if (id_pojo != null) {
+			PojoDao pojoDao = new PojoDao();
+			Pojo test = pojoDao.getPojo(id_pojo);
+			PojoToJson pojoToJson = new PojoToJson();
+			String printPojo = pojoToJson.toJson(test);
+			System.out.println(printPojo);
+
+			try (PrintWriter out = response.getWriter()) {
+				out.println(printPojo);
+			}
+		}
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "La première servlet .";
+
+	}
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
 		System.out.println("Init terminé");
 	}
-    
-    
+
 }
