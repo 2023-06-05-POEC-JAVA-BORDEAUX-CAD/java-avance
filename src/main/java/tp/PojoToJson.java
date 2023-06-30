894643afe730b5b0d8d926910108b7927391dbab
Fab <fabien@elixeer.fr>
2023-06-30 16:57:21 +0200

    Update PojoToJson.java
    Co-Authored-By: Angelisium <angelisium@daum.net>
    Co-Authored-By: Farid Chouchane <103304709+FaridChouchane@users.noreply.github.com>

--- src/main/java/tp/PojoToJson.java
+++ src/main/java/tp/PojoToJson.java
@@ -3,11 +3,15 @@ package tp;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+// récupère une instance de Pojo en param
+// retourne soit l'instance de Pojo au format JSON : {"name": "name here"} 
+// ou retourne null au format JSON : "null"
+
 public class PojoToJson {
-    public static String toJson(Pojo pojo) throws JsonProcessingException {
+    public static String toJson(Pojo pojo) { 
         try {
             return new ObjectMapper().writeValueAsString(pojo);
-        } catch (Exception e) {
+        } catch (JsonProcessingException e) {
             return "null";
         }
     }
