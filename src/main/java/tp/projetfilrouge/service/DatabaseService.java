bca9a0229bda32fc521c6463e3af8c61997e1d5c
Farid CHOUCHANE <chouchanefarid@gmail.com>
2023-07-05 15:31:02 +0200

    ajout 15h30

--- src/main/java/tp/projetfilrouge/service/DatabaseService.java
+++ src/main/java/tp/projetfilrouge/service/DatabaseService.java
@@ -6,13 +6,19 @@ import java.sql.SQLException;
 import java.sql.DriverManager;
 
 public class DatabaseService {
-	private final String DB_URL = "jdbc:mysql://127.0.0.1:3306?useSSL=false";
+	private final String DB_URL = "jdbc:mysql://127.0.0.1:3306/pfr?useSSL=false";
 	private final String DB_USER = "root";
 	private final String DB_PASSWORD = "";
 
 	public Statement db;
 
 	public DatabaseService() throws SQLException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Connection database = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 		db = database.createStatement();
 	}
