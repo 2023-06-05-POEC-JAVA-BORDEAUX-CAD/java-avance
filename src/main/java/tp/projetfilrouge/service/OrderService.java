649527807186f1a6638bf51beb18a5ed4dadab07
Angelisium <angelisium@daum.net>
2023-07-05 14:23:51 +0200

    Update OrderService.java

--- src/main/java/tp/projetfilrouge/service/OrderService.java
+++ src/main/java/tp/projetfilrouge/service/OrderService.java
@@ -1,22 +1,42 @@
 package tp.projetfilrouge.service;
 
 import java.util.List;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import jakarta.ejb.Stateless;
+import java.sql.SQLException;
+
+import tp.projetfilrouge.pojo.Client;
 import tp.projetfilrouge.pojo.Order;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Stateless
-public class OrderService {
+public class OrderService extends DatabaseService {
+
+	public OrderService() throws SQLException {
+		super();
+	}
+
 	public Order getById(Integer id) {
 		return new Order();
 	}
 
-	public List<Order> getAll() {
+	public List<Order> getAll() throws SQLException {
+		// SELECT * FROM `pfr`.`clients`
 		List<Order> orders = new ArrayList<Order>();
-		orders.add(new Order());
-		orders.add(new Order());
-		orders.add(new Order());
-		orders.add(new Order());
+		ResultSet result = db.executeQuery("SELECT * FROM `pfr`.`orders`");
+		while (result.next()) {
+			Order ord = new Order();
+			ord.setId(result.getInt("id"));
+			ord.setTypePresta(result.getString("type_presta"));
+			ord.setClientId(result.getInt("client_id"));
+			ord.setNbDays(result.getInt("nb_days"));
+			ord.setUnitPrice(result.getInt("unit_price"));
+			ord.setState(result.getInt("state"));
+			ord.setDesignation(result.getString("designation"));
+			orders.add(ord);
+		}
 		return orders;
 	}
 
@@ -24,8 +44,8 @@ public class OrderService {
 		return "{}";
 	}
 
-	public static String toJson(List<Order> orders) {
-		return "[{},{},{}]";
+	public static String toJson(List<Order> orders) throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(orders);
 	}
 
 }
