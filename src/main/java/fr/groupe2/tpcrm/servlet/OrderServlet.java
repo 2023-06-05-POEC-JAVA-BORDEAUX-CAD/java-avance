e868eea942a31994f8c4fd138f85039d671f63b3
philippe hassan <hassan.philippe@yahoo.com>
2023-07-05 14:52:57 +0200

    OrderServlet

--- src/main/java/fr/groupe2/tpcrm/servlet/OrderServlet.java
+++ src/main/java/fr/groupe2/tpcrm/servlet/OrderServlet.java
@@ -1,41 +1,67 @@
 package fr.groupe2.tpcrm.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.groupe2.tpcrm.service.OrderService;
+import jakarta.inject.Inject;
+
 /**
  * Servlet implementation class OrderServlet
  */
-@WebServlet(name="OrderServlet", urlPatterns="/clients", loadOnStartup=1)
+@WebServlet(name = "OrderServlet", displayName = "Order Servlet", urlPatterns = "/orders", loadOnStartup = 1)
 public class OrderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public OrderServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	@Inject
+	private OrderService orderService;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public OrderServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		try (PrintWriter out = response.getWriter()) {
+			String id = request.getParameter("id");
+			if (id != null) {
+				try {
+					Integer intId = Integer.valueOf(id);
+					String orderToString = orderService.getOrderByIdToString(intId);
+					if (orderToString != null) {
+						out.println("<p>" + orderToString + "</p>");
+					} else {
+						response.setStatus(404);
+						out.println("<p class=\"error\">Erreur 404</p>");
+					}
+				} catch (NumberFormatException e) {
+					out.println("<p class=\"error\">Le format de l'id est incorrect</p>");
+				}
+			} else {
+				out.println("<p>Affichage de la liste des commandes</p>");
+				String ordersToString = orderService.getOrdersToString();
 
+				if (ordersToString != null) {
+					out.println("<p>" + ordersToString + "</p>");
+				} else {
+					response.setStatus(404);
+					out.println("<p class=\"error\">Erreur 404</p>");
+				}
+			}
+		}
+	}
 }
