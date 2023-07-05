9c2dda15479ebb80d0dcb266190a8a329f025321
Ebru Jolivet <ebruozogul1@gmail.com>
2023-07-05 14:01:53 +0200

    client service

--- src/main/java/fr/groupe3/tpfilrouge/clients/ClientService.java
+++ src/main/java/fr/groupe3/tpfilrouge/clients/ClientService.java
@@ -1,5 +1,22 @@
 package fr.groupe3.tpfilrouge.clients;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.maboite.correction.pojojsonservlet.pojo.Pojo;
+import jakarta.ejb.Stateless;
+
+@Stateless
 public class ClientService {
 
+	
+		public String toJson(Pojo pojo) throws JsonProcessingException{
+			return new ObjectMapper().writeValueAsString(pojo);
+		}
+
+	}
+
+	
+	
+	
 }
