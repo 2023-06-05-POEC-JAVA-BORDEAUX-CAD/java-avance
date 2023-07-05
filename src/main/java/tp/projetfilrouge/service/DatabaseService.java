e780487435df0041e296ef10255ffc3683bbce64
Angelisium <angelisium@daum.net>
2023-07-05 14:07:15 +0200

    Create DatabaseService.java

---
+++ src/main/java/tp/projetfilrouge/service/DatabaseService.java
@@ -0,0 +1,19 @@
+package tp.projetfilrouge.service;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class DatabaseService {
+	private final String DB_URL = "jdbc:mysql://127.0.0.1:3306?useSSL=false";
+	private final String DB_USER = "root";
+	private final String DB_PASSWORD = "";
+
+	public Statement db;
+
+	public DatabaseService() throws SQLException {
+		Connection database = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		db = database.createStatement();
+	}
+}
