703e599f6639a20d850bf9936a5e2b7178e26e26
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-06 14:20:42 +0200

    update PremierFaceBean & premier-jsf

--- src/main/java/dev/loicmoreaux/jsf/controller/PremierFaceBean.java
+++ src/main/java/dev/loicmoreaux/jsf/controller/PremierFaceBean.java
@@ -11,11 +11,29 @@ import jakarta.inject.Named;
 @RequestScoped
 public class PremierFaceBean {
 	private static final String MESSAGE = "Un message qui provient d'une classe Java !";
+	private boolean myBoolean = true;
+	private Integer myInteger = 13;
 
 	public String getMessage() {
 		return MESSAGE;
 	}
 	
+	public boolean isMyBoolean() {
+		return myBoolean;
+	}
+
+	public void setMyBoolean(boolean myBoolean) {
+		this.myBoolean = myBoolean;
+	}
+
+	public Integer getMyInteger() {
+		return myInteger;
+	}
+
+	public void setMyInteger(Integer myInteger) {
+		this.myInteger = myInteger;
+	}
+
 	public String otherMessage() {
 		return "Un autre message";
 	}
