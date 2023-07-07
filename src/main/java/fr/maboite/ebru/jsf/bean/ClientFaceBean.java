1946ae80f252c07cf619c9f8a3c4defce8686b4d
Ebru Jolivet <ebruozogul1@gmail.com>
2023-07-07 12:20:54 +0200

    JSF2

--- src/main/java/fr/maboite/ebru/jsf/bean/ClientFaceBean.java
+++ src/main/java/fr/maboite/ebru/jsf/bean/ClientFaceBean.java
@@ -7,11 +7,26 @@ import jakarta.inject.Named;
 @RequestScoped
 public class ClientFaceBean {
 	
+	private String loadId;
+	
+	
+	public String getLoadId() {
+		return loadId;
+	}
+
+	public void setLoadId(String loadId) {
+		this.loadId = loadId;
+	}
+
 	public String createNew() {
 		System.out.println("client");
 		return "client";
 	}
-	
-	
+
+	public void onLoad() {
+		System.out.println("je suis en train de charger la vue");
+		System.out.println("je charge le client avec id: " + loadId);
+		
+	}
 
 }
