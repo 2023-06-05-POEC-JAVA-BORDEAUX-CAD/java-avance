b223aad21c69a542d87b5a713ea9f93c74e8f147
Angelisium <angelisium@daum.net>
2023-07-07 16:22:07 +0200

    Create ClientFaceBean.java

---
+++ src/main/java/tp/jsfClient/ClientFaceBean.java
@@ -0,0 +1,54 @@
+package tp.jsfClient;
+
+import jakarta.inject.Named;
+import jakarta.inject.Inject;
+import tp.projetfilrouge.pojo.Client;
+import jakarta.enterprise.context.RequestScoped;
+
+@Named
+@RequestScoped
+public class ClientFaceBean {
+	private String loadId;
+	private Client client = new Client();
+
+	@Inject
+	private ClientService beanClientService;
+
+	public String getLoadId() {
+		return loadId;
+	}
+
+	public void setLoadId(String loadId) {
+		this.loadId = loadId;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public ClientService getBeanClientService() {
+		return beanClientService;
+	}
+
+	public void setBeanClientService(ClientService beanClientService) {
+		this.beanClientService = beanClientService;
+	}
+
+	public String createNew() {
+		System.out.println("create new is called");
+		return "client";
+	}
+
+	public void onLoad() {
+		this.client = this.beanClientService.load(this.loadId);
+	}
+
+	public String save() {
+		this.beanClientService.save(this.client);
+		return "vueclient";
+	}
+}
