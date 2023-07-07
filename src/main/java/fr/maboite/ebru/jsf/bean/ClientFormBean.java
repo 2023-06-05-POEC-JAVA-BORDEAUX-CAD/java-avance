ebf00a7ca53f7fee12beba9e6d8b406c6ae74e67
Ebru Jolivet <ebruozogul1@gmail.com>
2023-07-07 17:01:54 +0200

    TPJSF fini

--- src/main/java/fr/maboite/ebru/jsf/bean/ClientFormBean.java
+++ src/main/java/fr/maboite/ebru/jsf/bean/ClientFormBean.java
@@ -1,27 +1,115 @@
 package fr.maboite.ebru.jsf.bean;
 
 public class ClientFormBean {
-	
-	private Integer id=3;
-	
-	private String companyName="M2i Formation";
 
-	
-	
-	
+	private Integer id = 3;
+
+	private String companyName = "M2i Formation";
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private String phone;
+
+	private String adress;
+
+	private String zipCode;
+
+	private String city;
+
+	private String country;
+
+	private Integer state;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getCompanyName() {
 		return companyName;
 	}
+
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
-	
-	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
 
 }
