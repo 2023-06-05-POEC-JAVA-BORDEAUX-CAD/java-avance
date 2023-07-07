032e0bff2d968aba7b1f16ad5d41cdfea6c892ad
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-07 14:44:56 +0200

    TP JSF add ClientFaceBean & ClientFormBean

---
+++ src/main/java/dev/loicmoreaux/tpjsf/controllers/ClientFaceBean.java
@@ -0,0 +1,10 @@
+package dev.loicmoreaux.tpjsf.controllers;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class ClientFaceBean {
+
+}
