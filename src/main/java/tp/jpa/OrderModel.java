9b061cead18adbdfec8d863221be0efbe73e0cfd
Angelisium <angelisium@daum.net>
2023-07-10 12:10:11 +0200

    Update OrderModel.java

---
+++ src/main/java/tp/jpa/OrderModel.java
@@ -0,0 +1,104 @@
+package tp.jpa;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "orders")
+public class OrderModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+
+	@Column(name = "type_presta")
+	String typePresta;
+
+	@Column(name = "designation")
+	String designation;
+
+	@Column(name = "client_id")
+	Integer clientId;
+
+	@Column(name = "nb_days")
+	Integer nbDays;
+
+	@Column(name = "unit_price")
+	Integer unitPrice;
+
+	@Column(name = "state")
+	Integer state;
+
+	public OrderModel() {
+	}
+
+	public OrderModel(Integer id, String typePresta, String designation, Integer clientId, Integer nbDays,
+			Integer unitPrice, Integer state) {
+		this.id = id;
+		this.typePresta = typePresta;
+		this.designation = designation;
+		this.clientId = clientId;
+		this.nbDays = nbDays;
+		this.unitPrice = unitPrice;
+		this.state = state;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTypePresta() {
+		return typePresta;
+	}
+
+	public void setTypePresta(String typePresta) {
+		this.typePresta = typePresta;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public Integer getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(Integer clientId) {
+		this.clientId = clientId;
+	}
+
+	public Integer getNbDays() {
+		return nbDays;
+	}
+
+	public void setNbDays(Integer nbDays) {
+		this.nbDays = nbDays;
+	}
+
+	public Integer getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Integer unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+}
