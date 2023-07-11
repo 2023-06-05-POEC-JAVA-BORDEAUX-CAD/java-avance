25103628731dd5a8bbaaf9314c083977fb18f174
Angelisium <angelisium@daum.net>
2023-07-11 14:58:23 +0200

    Update ClientModel.java

--- src/main/java/tp/jpa/ClientModel.java
+++ src/main/java/tp/jpa/ClientModel.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
@@ -11,40 +13,48 @@ import jakarta.persistence.GenerationType;
 @Table(name = "clients")
 public class ClientModel {
 	@Id
+	@Column(name = "id", nullable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer id;
 
-	@Column(name = "company_name")
+	@Column(name = "company_name", nullable = false, length = 40)
 	private String companyName;
 
-	@Column(name = "first_name")
+	@Column(name = "first_name", nullable = false, length = 40)
 	private String firstName;
 
-	@Column(name = "last_name")
+	@Column(name = "last_name", nullable = false, length = 40)
 	private String lastName;
 
-	@Column(name = "email")
+	@Column(name = "email", nullable = false, length = 90)
 	private String email;
 
-	@Column(name = "phone")
+	@Column(name = "phone", nullable = false, length = 10)
 	private String phone;
 
-	@Column(name = "address")
+	@Column(name = "address", nullable = false, length = 255)
 	private String adress;
 
-	@Column(name = "zip_code")
+	@Column(name = "zip_code", nullable = false, length = 10)
 	private String zipCode;
 
-	@Column(name = "city")
+	@Column(name = "city", nullable = false, length = 40)
 	private String city;
 
-	@Column(name = "country")
+	@Column(name = "country", nullable = false, length = 60)
 	private String country;
 
-	@Column(name = "state")
-	private Integer state;
+	@Enumerated(EnumType.ORDINAL) // EnumType.STRING
+	@Column(name = "state", nullable = false)
+	private State state;
 
 	public ClientModel() {
+	}
+
+	public enum State {
+		CANCELED,
+		OPTION,
+		CONFIRMED
 	}
 
 	public Integer getId() {
@@ -127,11 +137,11 @@ public class ClientModel {
 		this.country = country;
 	}
 
-	public Integer getState() {
+	public State getState() {
 		return state;
 	}
 
-	public void setState(Integer state) {
+	public void setState(State state) {
 		this.state = state;
 	}
 }
