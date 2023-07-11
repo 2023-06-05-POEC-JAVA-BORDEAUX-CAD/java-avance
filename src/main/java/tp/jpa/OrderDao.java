08e81f031d0bafac4c5d7af24a8041317f5a9161
Angelisium <angelisium@daum.net>
2023-07-11 11:43:29 +0200

    Update OrderDao.java

--- src/main/java/tp/jpa/OrderDao.java
+++ src/main/java/tp/jpa/OrderDao.java
@@ -9,7 +9,37 @@ public class OrderDao {
 	@PersistenceContext
 	protected EntityManager entityManager;
 
-	public void save(OrderModel client) {
-		this.entityManager.merge(client);
+	/**
+	 * Load a OrderModel by this identifier
+	 * 
+	 * @param id the OrderModel identifier
+	 * @return OrderModel
+	 */
+	public OrderModel load(Integer id) {
+		return this.entityManager.find(OrderModel.class, id);
+	}
+
+	/**
+	 * Save or update a OrderModel
+	 * 
+	 * @param order the OrderModel instance
+	 * @return OrderModel
+	 */
+	public OrderModel save(OrderModel order) {
+		return this.entityManager.merge(order);
+	}
+
+	/**
+	 * Delete a OrderModel by this identifier
+	 * 
+	 * @param id the OrderModel identifier
+	 * @return OrderModel
+	 */
+	public OrderModel delete(Integer id) {
+		OrderModel order = load(id);
+		if (order != null) {
+			this.entityManager.remove(order);
+		}
+		return order;
 	}
 }
