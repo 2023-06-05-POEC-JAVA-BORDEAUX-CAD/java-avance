861c738ec0e39a3745c89ebe6b03fa8885679882
Angelisium <angelisium@daum.net>
2023-07-11 11:43:26 +0200

    Update ClientDao.java

--- src/main/java/tp/jpa/ClientDao.java
+++ src/main/java/tp/jpa/ClientDao.java
@@ -9,7 +9,37 @@ public class ClientDao {
 	@PersistenceContext
 	protected EntityManager entityManager;
 
-	public void save(ClientModel client) {
-		this.entityManager.merge(client);
+	/**
+	 * Load a ClientModel by this identifier
+	 * 
+	 * @param id the ClientModel identifier
+	 * @return ClientModel
+	 */
+	public ClientModel load(Integer id) {
+		return this.entityManager.find(ClientModel.class, id);
+	}
+
+	/**
+	 * Save or update a ClientModel
+	 * 
+	 * @param client the ClientModel instance
+	 * @return ClientModel
+	 */
+	public ClientModel save(ClientModel client) {
+		return this.entityManager.merge(client);
+	}
+
+	/**
+	 * Delete a ClientModel by this identifier
+	 * 
+	 * @param id the ClientModel identifier
+	 * @return ClientModel
+	 */
+	public ClientModel delete(Integer id) {
+		ClientModel client = load(id);
+		if (client != null) {
+			this.entityManager.remove(client);
+		}
+		return client;
 	}
 }
