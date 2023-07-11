7496013422a2b8711a984b7b719e0b4081dad643
Noellie P <noellie.peuch@gmail.com>
2023-07-11 14:25:06 +0200

    Création de l'Entity clients

---
+++ src/main/java/fr/noellie/jpa/model/ClientJPA.java
@@ -0,0 +1,129 @@
+package fr.noellie.jpa.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class ClientJPA {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "company_name")
+	private String companyName;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	private String email;
+
+	private String phone;
+
+	private String adress;
+
+	@Column(name = "zip_code")
+	private String zipCode;
+
+	private String city;
+
+	private String country;
+
+	private Integer state;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+}
