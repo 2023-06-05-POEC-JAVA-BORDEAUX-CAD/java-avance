08970e5614fb84732684f16bfb585ccd386e2b59
Noellie P <noellie.peuch@gmail.com>
2023-07-11 14:45:22 +0200

    Création de la méthode sauvegardeClientJPA

--- src/main/java/fr/noellie/jsf/controller/MonPremierFaceBean.java
+++ src/main/java/fr/noellie/jsf/controller/MonPremierFaceBean.java
@@ -1,6 +1,8 @@
 package fr.noellie.jsf.controller;
 
+import fr.noellie.jpa.dao.ClientJPADao;
 import fr.noellie.jpa.dao.OrderJPADao;
+import fr.noellie.jpa.model.ClientJPA;
 import fr.noellie.jpa.model.OrderJPA;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
@@ -12,6 +14,9 @@ public class MonPremierFaceBean {
 
 	@Inject
 	private OrderJPADao orderJPADao;
+	
+	@Inject
+	private ClientJPADao clientJPADao;
 
 	private String message = "Ce message vient du controller MonPremierFaceBean";
 
@@ -27,9 +32,30 @@ public class MonPremierFaceBean {
 		OrderJPA savedOrder = orderJPADao.save(orderJPA);
 		
 		OrderJPA loadedOrder = orderJPADao.load(savedOrder.getId());
-		System.out.println("En base, j'ai un order dont la désignation vaut : " 
-				+ loadedOrder.getDesignation()
-				+ " pour l'id " + loadedOrder.getId());
+		System.out.println("En base, j'ai un order dont l'id est " 
+				+ loadedOrder.getId() + " et la désignation : "
+				+ loadedOrder.getDesignation());
+	}
+	
+	public void sauvegardeClientJPA() {
+		ClientJPA clientJPA = new ClientJPA();
+		clientJPA.setCompanyName("Mairie de Bayonne");
+		clientJPA.setFirstName("Patrick");
+		clientJPA.setLastName("Sébastien");
+		clientJPA.setEmail("patrick.sebastien@mail.fr");
+		clientJPA.setPhone("06 06 06 06 06");
+		clientJPA.setAdress("123 Rue des Lilas");
+		clientJPA.setZipCode("64100");
+		clientJPA.setCity("Bayonne");
+		clientJPA.setCountry("France");
+		clientJPA.setState(1);
+		ClientJPA savedClient = clientJPADao.save(clientJPA);
+		
+		ClientJPA loadedClient = clientJPADao.load(savedClient.getId());
+		System.out.println("En base, j'ai un client dont l'entreprise est " 
+				+ loadedClient.getCompanyName()
+				+ " et dont le nom complet est "
+				+ loadedClient.getFirstName() + " " + loadedClient.getLastName());
 	}
 
 }
