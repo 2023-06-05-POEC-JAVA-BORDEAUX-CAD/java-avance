c624b034fb194646e416079665025fd21a6d2672
Angelisium <angelisium@daum.net>
2023-07-12 12:05:52 +0200

    Update ClientDaoTest.java

--- src/test/java/tp/jpa/ClientDaoTest.java
+++ src/test/java/tp/jpa/ClientDaoTest.java
@@ -1,5 +1,6 @@
 package tp.jpa;
 
+import java.util.List;
 import jakarta.ejb.EJB;
 import java.util.Properties;
 import tp.jpa.ClientModel.State;
@@ -23,6 +24,31 @@ public class ClientDaoTest {
 		EjbJar ejb = new EjbJar("ordao");
 		ejb.addEnterpriseBean(new StatelessBean(ClientDao.class));
 		return ejb;
+	}
+
+	public ClientModel clientGenerator() {
+		String[] compname = { "Axa", "Total", "BNP", "Carrefour", "EDF" };
+		String[] firstname = { "Marie", "Julie", "Joris", "Olaf", "Dolfi" };
+		String[] lastname = { "Radolf", "Guerric", "Dionisio", "Kesari" };
+		String[] mail = { "na@wa.k", "nimp@orte.koi", "foo@bar.baz", "beer@pon.g" };
+		String[] phone = { "0417606761", "0300823836", "0136867849", "0483111478" };
+		String[] adress = { "15 Rue la Porte", "22 Avenue le clou", "3 boulevard zdedededex" };
+		String[] zip = { "XYZ", "70123", "75006", "33542", "64547", "40331" };
+		String[] city = { "Nawak", "Paris", "Gironde", "Landes", "Nopnop" };
+		String[] country = { "France" };
+		State[] states = { State.CANCELED, State.CONFIRMED, State.OPTION };
+		ClientModel client = new ClientModel();
+		client.setCompanyName(compname[(int) Math.floor(Math.random() * 5)]);
+		client.setFirstName(firstname[(int) Math.floor(Math.random() * 5)]);
+		client.setLastName(lastname[(int) Math.floor(Math.random() * 4)]);
+		client.setEmail(mail[(int) Math.floor(Math.random() * 4)]);
+		client.setPhone(phone[(int) Math.floor(Math.random() * 4)]);
+		client.setAdress(adress[(int) Math.floor(Math.random() * 3)]);
+		client.setZipCode(zip[(int) Math.floor(Math.random() * 6)]);
+		client.setCity(city[(int) Math.floor(Math.random() * 5)]);
+		client.setCountry(country[(int) Math.floor(Math.random() * 1)]);
+		client.setState(states[(int) Math.floor(Math.random() * 3)]);
+		return client;
 	}
 
 	@Module
@@ -49,17 +75,7 @@ public class ClientDaoTest {
 
 	@Test
 	public void save() {
-		ClientModel client = new ClientModel();
-		client.setCompanyName("NAME");
-		client.setFirstName("TestName");
-		client.setLastName("LastName");
-		client.setEmail("faketest@test.com");
-		client.setPhone("0542134302");
-		client.setAdress("Adresse de test");
-		client.setZipCode("XYZ");
-		client.setCity("VilleTest");
-		client.setCountry("FRANCE");
-		client.setState(State.CONFIRMED);
+		ClientModel client = clientGenerator();
 		ClientModel saved = clidao.save(client);
 
 		assertNotNull(client);
@@ -79,17 +95,7 @@ public class ClientDaoTest {
 
 	@Test
 	public void load() {
-		ClientModel client = new ClientModel();
-		client.setCompanyName("NAME");
-		client.setFirstName("TestName");
-		client.setLastName("LastName");
-		client.setEmail("faketest@test.com");
-		client.setPhone("0542134302");
-		client.setAdress("Adresse de test");
-		client.setZipCode("XYZ");
-		client.setCity("VilleTest");
-		client.setCountry("FRANCE");
-		client.setState(State.CONFIRMED);
+		ClientModel client = clientGenerator();
 		client = clidao.save(client);
 
 		ClientModel saved = clidao.load(client.getId());
@@ -101,17 +107,7 @@ public class ClientDaoTest {
 
 	@Test
 	public void delete() {
-		ClientModel client = new ClientModel();
-		client.setCompanyName("NAME");
-		client.setFirstName("TestName");
-		client.setLastName("LastName");
-		client.setEmail("faketest@test.com");
-		client.setPhone("0542134302");
-		client.setAdress("Adresse de test");
-		client.setZipCode("XYZ");
-		client.setCity("VilleTest");
-		client.setCountry("FRANCE");
-		client.setState(State.CONFIRMED);
+		ClientModel client = clientGenerator();
 		client = clidao.save(client);
 
 		ClientModel deleted = clidao.delete(client.getId());
@@ -119,5 +115,24 @@ public class ClientDaoTest {
 		assertNotNull(client);
 		assertNotNull(deleted);
 		assertEquals(client.getId(), deleted.getId());
+	}
+
+	@Test
+	public void getAllByCompanyNameTest() {
+		for (int i = 0; i < 5; i++) {
+			ClientModel client = clientGenerator();
+			if (i < 3) {
+				client.setCompanyName("CUSTOM NAME");
+			}
+			ClientModel saved = clidao.save(client);
+			assertNotNull(client);
+			assertNotNull(saved);
+		}
+
+		List<ClientModel> try1 = clidao.getAllByCompanyName("CUSTOM NAME");
+		List<ClientModel> try2 = clidao.getAllByCompanyName("NOP");
+
+		assertEquals(try1.size(), 3);
+		assertEquals(try2.size(), 0);
 	}
 }
