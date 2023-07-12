c59c245926be16f3eb4756428119a368edf1a877
Ebru Jolivet <ebruozogul1@gmail.com>
2023-07-12 10:55:48 +0200

    orderdao jpql

--- src/main/java/fr/maboite/ebru/jpa/entity/OrderDao.java
+++ src/main/java/fr/maboite/ebru/jpa/entity/OrderDao.java
@@ -1,8 +1,11 @@
 package fr.maboite.ebru.jpa.entity;
 
+import java.util.List;
+
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 
 //* Data Access Object: classe permettant 
 //* d'accéder en lecture ou écriture au référentiel 
@@ -23,4 +26,22 @@ public class OrderDao {
 		return this.entityManager.find(OrderJPA.class, id);
 	}
 
+	//equivalent requete sql: sleect*from orders where designation=...
+	
+	// renvoie tous les Orders qui ont comme designation designation
+	
+	public List<OrderJPA> findByDesignation(String argumentDeMethodedesignation) {
+		
+		Query jpqlQuery= this.entityManager.createQuery("select o "
+				+" from Orders o " 
+				+ " where o.designation = :parametreDeRequetedesignation", OrderJPA.class);
+		jpqlQuery.setParameter("parametreDeRequetedesignation", argumentDeMethodedesignation);
+		
+		
+		// o.designation = o.getdesignation, o est un alias, designation=nom de column
+		// setParameter, comme dans map a une clé (String) et une valeur objet(argument)
+		
+		return jpqlQuery.getResultList();
+	}
+	
 }
