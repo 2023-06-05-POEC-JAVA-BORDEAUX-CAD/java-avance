f4978352da4124f94b902bc186eee6d3860f0642
Angelisium <angelisium@daum.net>
2023-07-13 11:45:11 +0200

    Update ClientModel.java

--- src/main/java/tp/jpa/ClientModel.java
+++ src/main/java/tp/jpa/ClientModel.java
@@ -1,17 +1,14 @@
 package tp.jpa;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-
 import java.util.Set;
-
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
