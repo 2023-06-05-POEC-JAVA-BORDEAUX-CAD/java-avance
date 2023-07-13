252f5bf470565831f0f64219e834df63777bc0d5
Angelisium <angelisium@daum.net>
2023-07-13 14:54:12 +0200

    Update RestClientController.java

--- src/main/java/tp/rest/RestClientController.java
+++ src/main/java/tp/rest/RestClientController.java
@@ -1,34 +1,34 @@
 package tp.rest;
 
+import java.util.List;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import tp.jpa.ClientModel;
 import java.sql.SQLException;
-import java.util.List;
-
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.PathParam;
-import tp.projetfilrouge.pojo.Client;
-import tp.projetfilrouge.service.ClientService;
+import tp.jpa.ClientJpaService;
 
 @Stateless
 @Path("/rest/clients")
 @Produces("application/json")
 public class RestClientController {
 	@Inject
-	private ClientService cliserv;
+	private ClientJpaService cliserv;
 
 	@GET
-	@Produces("application/json")
-	public List<Client> getClients() throws SQLException {
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ClientModel> getClients() throws SQLException {
 		return cliserv.getAll();
 	}
 
 	@GET
 	@Path("/{id}")
-	@Produces("application/json")
-	public Client getClientById(@PathParam("id") Integer id) throws SQLException {
-		return cliserv.getById(id);
+	@Produces(MediaType.APPLICATION_JSON)
+	public ClientModel getClientById(@PathParam("id") Integer id) throws SQLException {
+		return cliserv.load(id);
 	}
 }
