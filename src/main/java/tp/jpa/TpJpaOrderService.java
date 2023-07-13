7ac5abe12d0d86f41e7144b577457cd39fb034fc
Angelisium <angelisium@daum.net>
2023-07-13 11:36:59 +0200

    Update TpJpaOrderService.java

---
+++ src/main/java/tp/jpa/TpJpaOrderService.java
@@ -0,0 +1,40 @@
+package tp.jpa;
+
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
+public class TpJpaOrderService {
+	@Inject
+	private OrderDao ordao;
+
+	/**
+	 * Load a OrderModel by this identifier
+	 * 
+	 * @param id the OrderModel identifier
+	 * @return OrderModel
+	 */
+	public OrderModel load(Integer id) {
+		return ordao.load(id);
+	}
+
+	/**
+	 * Save or update a OrderModel
+	 * 
+	 * @param order the OrderModel instance
+	 * @return OrderModel
+	 */
+	public OrderModel save(OrderModel order) {
+		return ordao.save(order);
+	}
+
+	/**
+	 * Delete a OrderModel by this identifier
+	 * 
+	 * @param id the OrderModel identifier
+	 * @return OrderModel
+	 */
+	public OrderModel delete(Integer id) {
+		return ordao.delete(id);
+	}
+}
