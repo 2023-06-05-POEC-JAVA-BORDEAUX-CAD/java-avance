75e602bcc4ed741c5a68fa0623ddb94c28772bf6
Angelisium <angelisium@daum.net>
2023-07-13 11:45:10 +0200

    Update ClientJpaService.java

---
+++ src/main/java/tp/jpa/ClientJpaService.java
@@ -0,0 +1,40 @@
+package tp.jpa;
+
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
+public class ClientJpaService {
+	@Inject
+	private ClientDao clidao;
+
+	/**
+	 * Load a ClientModel by this identifier
+	 * 
+	 * @param id the ClientModel identifier
+	 * @return ClientModel
+	 */
+	public ClientModel load(Integer id) {
+		return clidao.load(id);
+	}
+
+	/**
+	 * Save or update a ClientModel
+	 * 
+	 * @param client the ClientModel instance
+	 * @return ClientModel
+	 */
+	public ClientModel save(ClientModel client) {
+		return clidao.save(client);
+	}
+	
+	/**
+	 * Delete a ClientModel by this identifier
+	 * 
+	 * @param id the ClientModel identifier
+	 * @return ClientModel
+	 */
+	public ClientModel delete(Integer id) {
+		return clidao.delete(id);
+	}
+}
