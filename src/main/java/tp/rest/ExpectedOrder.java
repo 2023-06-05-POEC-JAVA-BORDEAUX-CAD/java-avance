5d61e9f2319709ed7c11fe49ce874be3892dc8b8
Angelisium <angelisium@daum.net>
2023-07-17 14:26:47 +0200

    Update ExpectedOrder.java

---
+++ src/main/java/tp/rest/ExpectedOrder.java
@@ -0,0 +1,78 @@
+package tp.rest;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class ExpectedOrder {
+	@NotNull
+	@Size(min = 3, max = 40)
+	private String typePresta;
+
+	@NotNull
+	@Size(min = 3, max = 90)
+	private String designation;
+
+	@NotNull
+	@Min(0)
+	private Integer clientId;
+
+	@Min(0)
+	private Integer nbDays;
+
+	@Min(0)
+	private Integer unitPrice;
+
+	@Min(0)
+	@Max(1)
+	private Integer state;
+
+	public String getTypePresta() {
+		return typePresta;
+	}
+
+	public void setTypePresta(String typePresta) {
+		this.typePresta = typePresta;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public Integer getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(Integer clientId) {
+		this.clientId = clientId;
+	}
+
+	public Integer getNbDays() {
+		return nbDays;
+	}
+
+	public void setNbDays(Integer nbDays) {
+		this.nbDays = nbDays;
+	}
+
+	public Integer getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Integer unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+}
