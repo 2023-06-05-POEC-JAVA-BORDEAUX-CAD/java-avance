937afc5f4a18c7051d04611f6815e554e9c87603
philippe hassan <hassan.philippe@yahoo.com>
2023-07-18 13:56:06 +0200

    controller client et order

--- src/main/java/fr/groupe2/tpapi/controller/ClientController.java
+++ src/main/java/fr/groupe2/tpapi/controller/ClientController.java
@@ -1,18 +1,16 @@
 package fr.groupe2.tpapi.controller;
 
 import fr.groupe2.tpapi.dto.ClientDto;
-import fr.groupe2.tpapi.dto.OrderDto;
 import fr.groupe2.tpapi.model.Client;
-import fr.groupe2.tpapi.model.Order;
 import fr.groupe2.tpapi.service.ClientService;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
-import jakarta.websocket.server.PathParam;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -40,43 +38,27 @@ public class ClientController {
 
 	@POST
 	@Path("/")
-	public Response createOrder(@Valid ClientDto clientDto) {
+	public Response createClient(@Valid ClientDto clientDto) {
 
 		if (clientDto == null) {
 			return Response.status(Status.BAD_REQUEST).build();
 		}
 
-
 		Client client = clientDto.toEntity();
-		if (clientDto.getClientId() != null) {
-			Order order = orderService.getById(clientDto.getClientId());
-
-			if (client == null) {
-				return Response.status(Status.BAD_REQUEST).build();
-			}
-			// le client correspond à un vrai client en base
-			client.setOrder(order);
-		}
-		
-		
-		
-		Client savedClientr = this.clientService.save(client);
+		Client savedClient = this.clientService.save(client);
 
 		return Response.ok(new ClientDto(savedClient)).build();
-
 	}
-	
-	
+
 	@DELETE
 	@Path("/{id}")
 	public Response delete(@PathParam("id") Integer id) {
-		Client client = this.clientService.load(id);
+		Client client = this.clientService.getClientById(id);
 		if (client == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		}
 		this.clientService.deleteById(id);
 		return Response.status(Status.OK).build();
 	}
-
 
 }
