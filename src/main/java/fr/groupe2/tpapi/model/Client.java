b6712a45fdece0befa16f2859c6e34791f01e25b
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-18 10:53:31 +0200

    update models tpapi

--- src/main/java/fr/groupe2/tpapi/model/Client.java
+++ src/main/java/fr/groupe2/tpapi/model/Client.java
@@ -1,5 +1,148 @@
 package fr.groupe2.tpapi.model;
 
-public class Client {
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "company_name", nullable = false)
+	private String companyName;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	private String email;
+	
+	private String phone;
+	
+	private String address;
+	
+	@Column(name = "zip_code")
+	private String zipCode;
+	
+	private String city;
+	
+	private String country;
+	
+	private Boolean state;
+	
+	@OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+	private List<Order> orders = new ArrayList<>();
+	
+	
+	/**
+	 *  Getters and Setters
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Boolean getState() {
+		return state;
+	}
+
+	public void setState(Boolean state) {
+		this.state = state;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
 }
--- src/main/java/fr/groupe2/tpapi/model/Order.java
+++ src/main/java/fr/groupe2/tpapi/model/Order.java
@@ -1,5 +1,96 @@
 package fr.groupe2.tpapi.model;
 
-public class Order {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "orders")
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "type_presta", nullable = false)
+	private String typePresta;
+	
+	@Column(nullable = false)
+	private String designation;
+	
+	@Column(name = "nb_days")
+	private Integer nbDays = null;
+	
+	@Column(name = "unit_price")
+	private Float unitPrice = null;
+	
+	private Integer state = null;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "client_id", nullable = false)
+	private Client client;
+	
+	/*
+	 * Getters and Setters
+	 */
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTypePresta() {
+		return typePresta;
+	}
+	
+	public void setTypePresta(String typePresta) {
+		this.typePresta = typePresta;
+	}
+	
+	public String getDesignation() {
+		return designation;
+	}
+	
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	public Integer getNbDays() {
+		return nbDays;
+	}
+	
+	public void setNbDays(Integer nbDays) {
+		this.nbDays = nbDays;
+	}
+	
+	public Float getUnitPrice() {
+		return unitPrice;
+	}
+	
+	public void setUnitPrice(Float unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	public Integer getState() {
+		return state;
+	}
+	
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
 }
