c789848374caedc22d6b27c6bef9031d284f87e8
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-07-18 10:58:50 +0200

    feature : add client & order DAO

--- src/main/java/fr/groupe2/tpapi/dao/ClientDao.java
+++ src/main/java/fr/groupe2/tpapi/dao/ClientDao.java
@@ -1,5 +1,56 @@
 package fr.groupe2.tpapi.dao;
 
+import java.util.List;
+
+import fr.groupe2.tpapi.model.*;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+
+@Stateless
 public class ClientDao {
 
+	@PersistenceContext
+	protected EntityManager entityManger;
+	
+	/**
+	 * Find an order object by id
+	 * @param id
+	 * @return Client
+	 */
+	public Client find(Integer id) {
+		return this.entityManger.find(Client.class, id);
+	}
+	
+	/**
+	 * Return all orders
+	 * @return List<Client>
+	 */
+	public List<Client> getAllOrders() {
+		TypedQuery<Client> query = this.entityManger.createQuery("select c from Client c", Client.class) ;
+		return query.getResultList();
+	}
+	
+	
+	/**
+	 * Saves a new and or an existent order
+	 * @param order
+	 * @return Client
+	 */
+	public Client save(Client order) {
+		return this.entityManger.merge(order);
+	}
+	
+	/**
+	 * Destroys an order
+	 * @param id
+	 */
+	public void delete(Integer id) {
+		Client orderTODelete = this.find(id);
+		this.entityManger.remove(orderTODelete);
+	}
+	
+	
 }
--- src/main/java/fr/groupe2/tpapi/dao/OrderDao.java
+++ src/main/java/fr/groupe2/tpapi/dao/OrderDao.java
@@ -1,5 +1,55 @@
 package fr.groupe2.tpapi.dao;
 
+import java.util.List;
+
+import fr.groupe2.tpapi.model.*;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+
+@Stateless
 public class OrderDao {
 
+	@PersistenceContext
+	protected EntityManager entityManger;
+	
+	/**
+	 * Find an order object by id
+	 * @param id
+	 * @return Client
+	 */
+	public Order find(Integer id) {
+		return this.entityManger.find(Order.class, id);
+	}
+	
+	/**
+	 * Return all orders
+	 * @return List<Client>
+	 */
+	public List<Order> getAllOrders() {
+		TypedQuery<Order> query = this.entityManger.createQuery("select o from Client o", Order.class) ;
+		return query.getResultList();
+	}	
+	
+	/**
+	 * Saves a new and or an existent order
+	 * @param order
+	 * @return Client
+	 */
+	public Client save(Client order) {
+		return this.entityManger.merge(order);
+	}
+	
+	/**
+	 * Destroys an order
+	 * @param id
+	 */
+	public void delete(Integer id) {
+		Order orderTODelete = this.find(id);
+		this.entityManger.remove(orderTODelete);
+	}
+	
+	
 }
