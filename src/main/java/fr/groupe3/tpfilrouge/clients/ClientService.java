e3757344993842fe9f4e4d736f2b9d48d3830cb2
Ebru Jolivet <ebruozogul1@gmail.com>
2023-07-18 12:37:24 +0200

    ClientService

--- src/main/java/fr/groupe3/tpfilrouge/clients/ClientService.java
+++ src/main/java/fr/groupe3/tpfilrouge/clients/ClientService.java
@@ -1,5 +1,31 @@
 package fr.groupe3.tpfilrouge.clients;
 
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
 public class ClientService {
+	
+	@Inject
+	private ClientDao clientDao;
+	
+	
+	public Client save(Client client) {
+		System.out.println("Sauvegarde de : " + client);
+		return this.clientDao.save(client);
+	}
+	
+	
+	 
+	public Client load(Long id) {
+		System.out.println("Chargement de order avec id : " + id);
+		Client client = this.clientDao.load(id);
+		if(client == null) {
+			System.out.println("Aucun order trouvé");
+		}else {
+			System.out.println("L'order trouvé est : " + client);
+		}
+		return client;
+	}
 
 }
