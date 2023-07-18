a27cdac02cd2e541a0546bdf62e92d10c765aea3
philippe hassan <hassan.philippe@yahoo.com>
2023-07-18 11:58:25 +0200

    controller

--- src/main/java/fr/groupe2/tpapi/controller/ClientController.java
+++ src/main/java/fr/groupe2/tpapi/controller/ClientController.java
@@ -1,5 +1,66 @@
 package fr.groupe2.tpapi.controller;
 
+import fr.groupe2.tpapi.dto.ClientDto;
+import fr.groupe2.tpapi.model.Client;
+import fr.groupe2.tpapi.model.Order;
+import fr.groupe2.tpapi.service.ClientService;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Stateless
+@Path("/clients")
+@Produces(MediaType.APPLICATION_JSON)
 public class ClientController {
+
+	@Inject
+	private ClientService clientService;
+
+	@GET
+	@Path("/{id}")
+	public Response getClientById(@PathParam("id") Integer id) {
+		// Appel du service pour récupérer
+		Client client = this.clientService.load(id);
+		if (client == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		// client!=null
+		return Response.ok(new ClientDto(client)).build();
+	}
+
+	@POST
+	@Path("/")
+	public Response createOrder(@Valid ClientDto clientDto) {
+
+		if (clientDto == null) {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+
+		Client savedClientr = this.clientService.save(client);
+
+		return Response.ok(new OrderDto(savedClient)).build();
+	}
+	
+	
+	@DELETE
+	@Path("/{id}")
+	public Response delete(@PathParam("id") Integer id) {
+		Client client = this.clientService.load(id);
+		if (client == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		this.clientService.delete(id);
+		return Response.status(Status.OK).build();
+	}
+
 
 }
--- src/main/java/fr/groupe2/tpapi/controller/OrderController.java
+++ src/main/java/fr/groupe2/tpapi/controller/OrderController.java
@@ -1,5 +1,73 @@
 package fr.groupe2.tpapi.controller;
 
+import fr.groupe2.tpapi.dto.OrderDto;
+import fr.groupe2.tpapi.model.Order;
+import fr.groupe2.tpapi.service.OrderService;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/orders")
+@Produces(MediaType.APPLICATION_JSON)
 public class OrderController {
+
+	@Inject
+	private OrderService orderService;
+
+	@GET
+	@Path("/{id}")
+	public Response getOrderById(@PathParam("id") Integer id) {
+		// Appel du service pour récupérer
+		Order order = this.orderService.load(id);
+		if (order == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		// order!=null
+		return Response.ok(new OrderDto(order)).build();
+	}
+
+	@POST
+	@Path("/")
+	public Response createOrder(@Valid OrderDto orderDto) {
+
+		if (orderDto == null) {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+
+		Order order = orderDto.toEntity();
+		if (orderDto.getClientId() != null) {
+			Client client = clientService.getById(orderDto.getClientId());
+
+			if (client == null) {
+				return Response.status(Status.BAD_REQUEST).build();
+			}
+			// le client correspond à un vrai client en base
+			order.setClient(client);
+		}
+		Order savedOrder = this.orderService.save(order);
+
+		return Response.ok(new OrderDto(savedOrder)).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Response delete(@PathParam("id") Integer id) {
+		Order order = this.orderService.load(id);
+		if (order == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		this.orderService.delete(id);
+		return Response.status(Status.OK).build();
+	}
 
 }
