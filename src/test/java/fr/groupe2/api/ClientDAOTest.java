da671e41f261a461595fd33a6a2c05ba8424d0b4
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-07-18 13:22:34 +0200

    Test: add client dao test

---
+++ src/test/java/fr/groupe2/api/ClientDAOTest.java
@@ -0,0 +1,115 @@
+package fr.groupe2.api;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.openejb.jee.EjbJar;
+import org.apache.openejb.jee.StatelessBean;
+import org.apache.openejb.jee.jpa.unit.PersistenceUnit;
+import org.apache.openejb.junit5.RunWithApplicationComposer;
+import org.apache.openejb.testing.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fr.groupe2.tpapi.dao.*;
+import fr.groupe2.tpapi.model.*;
+import jakarta.ejb.EJB;
+
+/**
+ * 
+ * @author N Duquesne
+ *
+ */
+@RunWithApplicationComposer
+public class ClientDAOTest {
+
+	@EJB
+	private ClientDao clientDao;
+	
+	@EJB
+	private OrderDao orderDAO;
+
+	@org.apache.openejb.testing.Module
+	public EjbJar beans() {
+		EjbJar ejbJar = new EjbJar("my-beans");
+		ejbJar.addEnterpriseBean(new StatelessBean(ClientDao.class));
+		ejbJar.addEnterpriseBean(new StatelessBean(OrderDao.class));
+		return ejbJar;
+	}
+	
+    @org.apache.openejb.testing.Module
+    public PersistenceUnit persistence() {
+        PersistenceUnit unit = new PersistenceUnit("PersisterPU");
+        unit.setJtaDataSource("jtaTestDataSource");
+        unit.setNonJtaDataSource("jtaTestDataSourceUnManaged");
+        unit.addClass(Client.class);
+        unit.addClass(Order.class);
+        unit.setExcludeUnlistedClasses(true);
+        unit.setProperty("openjpa.jdbc.SynchronizeMappings", "buildSchema(ForeignKeys=true)");
+        unit.setProperty("openjpa.Log", "DefaultLevel=WARN,Runtime=INFO,Tool=INFO,SQL=TRACE");
+        return unit;
+    }
+    
+    @Configuration
+    public Properties config() throws Exception {
+        Properties p = new Properties();
+        p.put("jtaTestDataSource", "new://Resource?type=DataSource");
+        p.put("jtaTestDataSource.JdbcDriver", "org.h2.Driver");
+        p.put("jtaTestDataSource.username", "test");
+        p.put("jtaTestDataSource.password", "test");
+        p.put("jtaTestDataSource.JdbcUrl", "jdbc:h2:file:G:/moi/Documents/M2i/modules/h2-data/formation-poe-java-2023-07-11");
+        return p;
+    }
+    
+	@Test
+	public void testSave() throws Exception {
+		
+		//Arrange
+		String designation = "Harribo";
+		
+		
+		//Act
+		
+		Client client = new Client();
+		client.setCompanyName(designation);
+		Client savedClient = this.clientDao.save(client);
+		
+		Order order = new Order();
+		order.setClient(savedClient);
+		
+		Order savedOrder = this.orderDAO.save(order);
+		
+		
+		//Assert
+		Assertions.assertNotNull(savedClient);
+		Assertions.assertNotNull(savedOrder);
+		
+		Assertions.assertNotNull(savedClient.getOrders());
+		Assertions.assertEquals(designation, savedClient.getCompanyName());
+		Assertions.assertEquals(1,savedClient.getId());
+		Assertions.assertEquals(1, savedClient.getOrders().size());
+	}
+	
+	
+	@Test
+	public void testDeleteById() throws Exception {
+		//Arrange
+		String compName = "Harribo";
+		Client client = new Client();
+		client.setCompanyName(compName);
+		
+		Client savedOrder1 = this.clientDao.save(client);
+		Client savedOrder2 = this.clientDao.save(client);
+		
+		Assertions.assertNotNull(savedOrder1);
+		Assertions.assertNotNull(savedOrder2);
+		
+		//Act
+		
+		this.clientDao.delete(savedOrder2.getId());
+		
+		Client clientGet = this.clientDao.find(savedOrder2.getId());
+		
+		Assertions.assertNull(clientGet);
+	}
+	
+}
